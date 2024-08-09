14c4d333a9d8bc043c2e1551519d766ac8fbd47b
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-08-09 23:10:49 +0300

    delete user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,9 @@ class UserController extends Controller
     public function delete($id)
     {
         $user = User::findOrFail($id);
+        if(!$user){
+            return response()->json(['message' => 'User not found'], 404);
+        }
         $user->delete();
         return response()->json(['message' => 'User deleted successfully'], 200);
     }
