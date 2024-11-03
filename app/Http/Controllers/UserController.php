b23795400f1efc47b7dd973a5caaf23cfa2c4ce8
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2024-11-03 21:19:13 +0200

    tets

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 use App\Jobs\ProcessExampleJob;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     public function index()
@@ -23,4 +26,23 @@ class UserController extends Controller
         return response()->json(['message' => 'User deleted successfully'], 200);
     }
 
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user()->id;
+        if(!$user){
+            return response()->json(['message' => 'User not found'], 404);
+        }
+        if($request->password){
+           $password = Hash::make($request->password);
+           $request->merge(['password' => $password]);
+        }
+        $user->update($request->all());
+        return response()->json(['message' => 'User updated successfully'], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('delete-users/{id}', [\App\Http\Controllers\UserController::class, 'delete']);
+    Route::post('update-user', [\App\Http\Controllers\UserController::class, 'update']);
 });
 
 
