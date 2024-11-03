a50483bdffa7f51ca66413cbe2ba264ccba41f39
samiralaa <devsamiralzeer2432gmail.com>
2024-11-03 21:29:56 +0300

    tets

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CardController;
 
 use App\Http\Controllers\CardLinkController;
